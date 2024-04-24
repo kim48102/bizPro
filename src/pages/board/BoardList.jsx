8a9ff1f63de3937e4988b75e6bff052adc11ed2d
biz1 <bizsystemtax@naver.com>
2024-04-24 11:32:23 +0900

    20240424_4

--- src/pages/board/BoardList.jsx
+++ src/pages/board/BoardList.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import URL from 'constants/url';
 
 function BoardList(){
     return(
-        <div>
-            <h1>게시판 목록</h1>
+        <div className="container">
+            <div className="c_wrap">
+                {/* <!-- Location --> */}
+                <div className="location">
+                    <ul>
+                        <li>Home</li>
+                    </ul>
+                </div>
+                {/* <!--// Location --> */}
+
+                <div className="layout">
+
+                    <div className="contents NOTICE_LIST" id="contents">
+                        {/* <!-- 본문 --> */}
+
+                        <div className="top_tit">
+                            <h1 className="tit_1">자유게시판</h1>
+                        </div>
+
+                        {/* <!-- 게시판목록 --> */}
+                        <div className="board_list BRD002">
+                            <div className="head">
+                                <span>글번호</span>
+                                <span>제목</span>
+                                <span>작성자</span>
+                                <span>작성일</span>
+                                <span>조회수</span>
+                            </div>
+                            <div className="result">
+                                <span>1</span>
+                                <span>2</span>
+                                <span>3</span>
+                                <span>4</span>
+                                <span>5</span>
+                            </div>
+                        </div>
+                        {/* <!--// 게시판목록 --> */}
+
+
+                        {/* <!--// 본문 --> */}
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default BoardList;
